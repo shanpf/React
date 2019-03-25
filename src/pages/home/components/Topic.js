70d7489cd2bb6cabf7651d3c65084bce26b38c66
shanpf <shanpf@yonyou.com>
2019-03-25 17:11:30 +0800

    home的简单布局2
    Signed-off-by: shanpf <shanpf@yonyou.com>

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import { TopicWrapper, TopicItem } from '../style'
 
 class Topic extends Component {
     render() {
         return(
-            <div>Topic</div>
+            <TopicWrapper>
+                <TopicItem>
+                <img
+                    className='topic-pic'
+                    src='//upload-images.jianshu.io/upload_images/5967942-a27900c2cb601e29?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+                />
+                流浪地球
+            </TopicItem>
+            <TopicItem>
+                <img
+                    className='topic-pic'
+                    src='//upload-images.jianshu.io/upload_images/5967942-a27900c2cb601e29?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+                />
+                流浪地球
+            </TopicItem>
+            <TopicItem>
+                <img
+                    className='topic-pic'
+                    src='//upload-images.jianshu.io/upload_images/5967942-a27900c2cb601e29?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+                />
+                流浪地球
+            </TopicItem>
+            <TopicItem>
+                <img
+                    className='topic-pic'
+                    src='//upload-images.jianshu.io/upload_images/5967942-a27900c2cb601e29?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+                />
+                流浪地球
+            </TopicItem>
+            </TopicWrapper>
         );
     }
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -21,3 +21,30 @@ export const HomeRight = styled.div`
     float: right;
     width: 240px;
 `;
+
+export const TopicWrapper = styled.div`
+    overflow: hidden;
+    padding: 20px 0 10px 0;
+    margin-left: -18px;
+`;
+
+export const TopicItem = styled.div`
+    float: left;
+    background: #f7f7f7;
+    padding-right: 10px;
+    margin-left: 18px;
+    height: 32px;
+    line-height:32px;
+    font-size: 14px;
+    color: #000;
+    border:1px  solid #dcdcdc;
+    border-radius: 4px;
+    .topic-pic{
+        display: block;
+        float: left;
+        height: 32px;
+        width: 32px;
+        margin-right: 10px;
+        margin-bottom: 18px;
+    }
+`;
