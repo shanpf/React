f1f51ede93949c6e43fb31f124e71f82dd8d585c
shanpf <shanpf@yonyou.com>
2019-03-25 15:50:44 +0800

    home的简单布局
    Signed-off-by: shanpf <shanpf@yonyou.com>

---
+++ src/pages/home/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const HomeWrapper = styled.div`
+    overflow: hidden;
+    width: 960px;
+    margin: 0 auto;
+`;
+
+export const HomeLeft = styled.div`
+    float: left;
+    margin-top: 30px;
+    margin-left: 15px;
+    width: 625px;
+    .banner-img{
+        width: 625px;
+        height: 270px;
+    }
+`;
+
+export const HomeRight = styled.div`
+    float: right;
+    width: 240px;
+`;
